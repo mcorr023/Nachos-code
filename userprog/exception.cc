847938b506e0205d3eef31f67845394bea7b90cb
rjime071 <99309702+rjime071@users.noreply.github.com>
2022-11-16 22:00:54 -0500

    Update exception.cc

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -117,39 +117,41 @@ void childFunction(int pid) {
 int doFork(int functionAddr) {
 
     // 1. Check if sufficient memory exists to create new process
-    // currentThread->space->GetNumPages() <= mm->GetFreePageCount()
+    if (!(currentThread->space->GetNumPages() <= mm->GetFreePageCount())){
+        return -1
+    }
     // if check fails, return -1
 
     // 2. SaveUserState for the parent thread
-    // currentThread->SaveUserState();
+    currentThread->SaveUserState();
 
     // 3. Create a new address space for child by copying parent address space
-    // Parent: currentThread->space
-    // childAddrSpace: new AddrSpace(currentThread->space)
+    Parent: currentThread->space
+    childAddrSpace: new AddrSpace(currentThread->space)
 
     // 4. Create a new thread for the child and set its addrSpace
-    // childThread = new Thread("childThread")
-    // child->space = childAddSpace;
+    childThread = new Thread("childThread")
+    child->space = childAddSpace;
 
     // 5. Create a PCB for the child and connect it all up
-    // pcb: pcbManager->AllocatePCB();
-    // pcb->thread = childThread
+    pcb: pcbManager->AllocatePCB();
+    pcb->thread = childThread
     // set parent for child pcb
     // add child for parent pcb
 
     // 6. Set up machine registers for child and save it to child thread
-    // PCReg: functionAddr
-    // PrevPCReg: functionAddr-4
-    // NextPCReg: functionAddr+4
-    // childThread->SaveUserState();
+    PCReg: functionAddr
+    PrevPCReg: functionAddr-4
+    NextPCReg: functionAddr+4
+    childThread->SaveUserState();
 
     // 7. Restore register state of parent user-level process
-    // currentThread->RestoreUserState()
+    currentThread->RestoreUserState()
 
     // 8. Call thread->fork on Child
-    // childThread->Fork(childFunction, pcb->pid)
+    childThread->Fork(childFunction, pcb->pid)
 
-    // 9. return pcb->pid;
+    return pcb->pid;
 
 }
 
@@ -243,14 +245,16 @@ int doJoin(int pid) {
 int doKill (int pid) {
 
     // 1. Check if the pid is valid and if not, return -1
-    //PCB* joinPCB = pcbManager->GetPCB(pid);
-    //if (pcb == NULL) return -1;
+    PCB* joinPCB = pcbManager->GetPCB(pid);
+    if (pcb == NULL) {
+         return -1;
+    }
 
     // 2. IF pid is self, then just exit the process
-    //if (pcb == currentThread->space->pcb) {
-    //       doExit(0);
-    //       return 0;
-    //}
+    if (pcb == currentThread->space->pcb) {
+          doExit(0);
+          return 0;
+    }
 
     // 3. Valid kill, pid exists and not self, do cleanup similar to Exit
     // However, change references from currentThread to the target thread
